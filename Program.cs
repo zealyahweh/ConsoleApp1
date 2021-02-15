da386dbd781bbd26201810993ed0f66eb23ca1ed
bgli100 <bgli100@126.com>
2021-02-16 00:24:20 +0800

    introduce System.Random as per Mono used by DSP

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp2
         static void Main()
         {
             int GalaxySeed = 14171500;
-            System.Random random = new System.Random(GalaxySeed);
+            Random random = new Random(GalaxySeed);
             random.Next();
             random.NextDouble();
             random.NextDouble();
@@ -15,7 +15,8 @@ namespace ConsoleApp2
             random.NextDouble();
             int StarSeed = random.Next();
             Console.WriteLine(StarSeed);
-            //Should Be 1826783713
+            Console.WriteLine("Should Be 1826783713");
+            Console.ReadLine();
         }
 
     }
